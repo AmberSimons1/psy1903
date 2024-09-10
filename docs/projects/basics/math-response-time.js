de975bea8f241ca04f3f78569e18707a8156a8a5
Amber Simons <susanbuck@college.harvard.edu>
2024-09-10 17:04:24 -0400

    Math Response Time

---
+++ docs/projects/basics/math-response-time.js
@@ -0,0 +1,18 @@
+alert(`
+In this experiment we will measure your response time.
+You will be shown a series of simple math equations. 
+Answer these equations as quickly and accurately as you can.`);
+
+let question1 = prompt("What is 3 + 4");
+
+alert("You answered " + question1);
+
+let question2 = prompt("What is 4 + 2");
+
+alert("You answered " + question2);
+
+let question3 = prompt("What is 7 + 1");
+
+alert("You answered " + question3);
+
+alert("Thank you for your participation");
