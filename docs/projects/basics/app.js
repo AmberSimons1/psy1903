b81612a33c55d073349f8c4f4dec08164ae00567
Amber Simons <susanbuck@college.harvard.edu>
2024-09-10 16:17:01 -0400

    Creating Outside Resources

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,28 +1,6 @@
-//Define a variable to hold our experiment name
-let experiment = 'Stroop';
-let trialCountMax = 20;
-let colors = ['red', 'green', 'blue'];
-let welcomeMessage = `
-Hello 1 edjsdk sasjOI 
-ASfopw` + experiment + `fas0w-0 
-00$$$$$$ 23804
-`;
+let response = prompt('What is 5 + 3');
+console.log(response);
 
-//console.log(welcomeMessage)
+let response2 = prompt('What is 5 + 5');
+console.log(response2);
 
-//alert('Welcome to the ' + experiment + ' experiment');
-//console.log(typeof experiment);
-//console.log(experiment.charAt(0));
-
-trialCountMax = 40;
-//console.log(typeof trialCountMax);
-
-//At the halfway point we will display a pause screen
-let halfWayCount = trialCountMax / 2;
-
-//console.log(halfWayCount); // expected 20
-
-let correct = true;
-console.log(typeof correct);
-
-console.log(10 > 15); // false 
