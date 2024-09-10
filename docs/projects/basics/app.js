07c98f5a4d316b51ee6f9272e0cabd88df9f7851
Amber Simons <susanbuck@college.harvard.edu>
2024-09-10 16:55:29 -0400

    Question 5

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,6 +1,14 @@
-let response = prompt('What is 5 + 3');
-console.log(response);
+let courseName = 'Programming for Psychologists';
+console.log(courseName.length); // Output: 29
 
-let response2 = prompt('What is 5 + 5');
-console.log(response2);
+// Output the value for courseName translated to all capital letters.
+// Expected result: PROGRAMMING FOR PSYCHOLOGISTS
+console.log([courseName.toUpperCase()]);
 
+// Output the index position of the word 'Psychologists' within the string stored in courseName.
+// Expected result: 16
+console.log([courseName.indexOf("Psychologists")]);
+
+// Output the value for courseName with the word 'Programming' replaced with the word 'Coding'.
+// Expected result: "Coding for Psychologists"
+console.log([courseName.replace("Programming", "Coding")]);
