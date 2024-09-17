3c6d0584944f6fe55d25de019eb3579ff8aefaa2
Amber Simons <susanbuck@college.harvard.edu>
2024-09-17 16:33:21 -0400

    Misc Practice A

---
+++ docs/projects/week3-misc-practice/app.js
@@ -0,0 +1,21 @@
+// best solution for doing this 
+
+let num1 = Math.floor(Math.random() * 10);
+let num2 = Math.floor(Math.random() * 10);
+let correctAnswer = num1 + num2;
+
+let response = prompt("What is " + num1 + " + " + num2 + "?");
+
+let feedback = ""
+
+if (response == correctAnswer) {
+    feedback = "You got it correct! ";
+} else if (response == (correctAnswer + 1) || response == (correctAnswer - 1)) {
+    feedback = "You were close! ";
+}
+else {
+    feedback = "You got it incorrect. ";
+}
+
+alert(feedback + "The expected answer is " + correctAnswer + "!");
+
