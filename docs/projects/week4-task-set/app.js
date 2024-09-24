93279406e9e3afa2afeb77104d6fbb3a1e5a4ea8
Amber Simons <susanbuck@college.harvard.edu>
2024-09-24 17:10:16 -0400

    convertTemp

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,7 +1,23 @@
 
+/*
+//Celsius to Fahrenheit
 function celsiusToFahrenheit(celsius) {
     return (celsius * 1.8) + 32;
 }
 
 console.log(celsiusToFahrenheit(10)); //50
 console.log(celsiusToFahrenheit(-5)); //23
+*/
+
+
+//Convert Temp
+function convertTemp(temp, convertTo) {
+
+    if (convertTo == 'c') {
+        return (temp - 32) / 1.8
+    } else {
+        return (temp * 1.8) + 32
+    }
+}
+console.log(convertTemp(10, 'c')); //-12.222222222222221
+console.log(convertTemp(10, 'f')); //50
