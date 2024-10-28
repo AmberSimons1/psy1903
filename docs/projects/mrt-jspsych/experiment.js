86df14cc654070195d264a5e4a09d5841d2639de
Amber Simons <susanbuck@college.harvard.edu>
2024-10-28 14:18:08 -0400

    Practice

--- docs/projects/mrt-jspsych/experiment.js
+++ docs/projects/mrt-jspsych/experiment.js
@@ -107,7 +107,7 @@ let resultsTrial = {
         //  ⭐ Update the following three values as appropriate ⭐
         let prefix = 'mrt';
         let dataPipeExperimentId = 'UKOmlhbKicSb';
-        let forceOSFSave = true;
+        let forceOSFSave = false;
 
         // Filter and retrieve results as CSV data
         let results = jsPsych.data
@@ -119,28 +119,9 @@ let resultsTrial = {
         // Generate a participant ID based on the current timestamp
         let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
 
-        // Dynamically determine if the experiment is currently running locally or on production
-        let isLocalHost = window.location.href.includes('localhost');
+        let fileName = prefix + '-' + participantId + '.csv';
 
-        let destination = '/save';
-        if (!isLocalHost || forceOSFSave) {
-            destination = 'https://pipe.jspsych.org/api/data/';
-        }
-
-        // Send the results to our saving end point
-        fetch(destination, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: '*/*',
-            },
-            body: JSON.stringify({
-                experimentID: dataPipeExperimentId,
-                filename: prefix + '-' + participantId + '.csv',
-                data: results,
-            }),
-        }).then(data => {
-            console.log(data);
+        saveResults(fileName, results, dataPipeExperimentId, forceOSFSave).then(response => {
             jsPsych.finishTrial();
         })
     }
